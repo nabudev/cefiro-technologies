02d82fb78025427db63c4515d1488d7aa6d9679e
nahueldev <nabudev01@gmail.com>
2024-09-24 02:26:51 -0300

    boton de whatsapp para contacto

--- myapp/src/app/page.js
+++ myapp/src/app/page.js
@@ -5,12 +5,14 @@ import { ContactFormJsx } from "@/components/contact-form";
 import { FooterJsx } from "@/components/footer";
 import { WhatWeDo } from "@/components/what-we-do";
 import { ProjectCarousel } from "@/components/project-carousel";
+import { WhatsAppButtonJsx } from "@/components/whats-app-button";
 import React, { useState } from 'react';
 
 export default function Home(){
   const [activeSection, setActiveSection] = useState('home');
   return(
     <>
+      <WhatsAppButtonJsx />
       <NavbarJsx activeSection={activeSection} setActiveSection={setActiveSection} />
       <section id="home">
         <HeroJsx />
